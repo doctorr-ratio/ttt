74ecdce6e024420f4160c567c9769cebcbc282b5
m <cyganov.mv15@physics.msu.ru>
2022-07-15 19:25:08 +0300

    minor fix to t1

--- test_task_1.py
+++ test_task_1.py
@@ -10,9 +10,9 @@ def task_simple(array: string):
 def task_fast(array: string):
     '''Let's realize binary search'''
     length = len(array)
-    mid = length // 2
     left = 0
-    right = length - 1
+    right = length
+    mid = (right + left) // 2
 
     # Under assumption that array with no 0 shoud return -1
     if length == 0 or array[-1] == '1': # not in, beacuse it's O(N)
@@ -21,15 +21,14 @@ def task_fast(array: string):
         return 0
 
     # Binary search for first zero
-    while array[mid] != '0' and array[mid - 1] != '1':
+    while not (array[mid] == '0' and array[mid - 1] == '1'):
+        mid = (right + left) // 2
         if array[mid] == '1':
-            mid = (mid + right) / 2
-            left = mid
+            left = mid + 1
         else:
-            mid = (mid + left) / 2
             right = mid - 1
 
-    return mid - 1
+    return mid
 
 # Let's make some tests
 
@@ -39,6 +38,8 @@ tests = [
     {"data": "00000", "answer": 0},
     {"data": "1", "answer": -1},
     {"data": "11111", "answer": -1},
+    {"data": "10000", "answer": 1},
+    {"data": "1110", "answer": 3},
     {"data": "1111110000000000000", "answer": 6}
 ]
 
@@ -47,9 +48,10 @@ if __name__ == '__main__':
     for i, test in enumerate(tests):
        test_answer = task_simple(test['data'])
        assert test_answer == test['answer'], f'Error on test case {i} for simple solution, got {test_answer}, expected {test["answer"]}'
-
+    print('DONE!')
 
     print("Tests for fast solution:")
     for i, test in enumerate(tests):
        test_answer = task_fast(test['data'])
        assert test_answer == test['answer'], f'Error on test case {i} for fast solution, got {test_answer}, expected {test["answer"]}'
+    print('DONE!')
